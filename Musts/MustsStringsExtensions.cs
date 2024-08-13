11f5f59b98f707cd2f0975e2109ea2d26f0ebea3
Simon Jefferies <simon@olivegame.studio>
2024-08-13 12:58:35 +0100

    Add string extensions.

---
+++ Musts/MustsStringsExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace Musts;
+
+public static class MustsStringsExtensions
+{
+    public static void MustBeNullOrEmpty(this string value)
+    {
+        if (!string.IsNullOrEmpty(value))
+        {
+            throw new MustsException();
+        }
+    }
+
+    public static void MustBeSame(this string value, string result)
+    {
+        if (value != result)
+        {
+            throw new MustsException();
+        }
+    }
+}
